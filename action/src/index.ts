7cc5c2e9f8c18c8b2a0f661bf58f5a656c06b74c
Ian Lewis <ianmlewis@gmail.com>
2023-05-22 06:41:17 +0000

    trw test
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

---
+++ action/src/index.ts
@@ -0,0 +1,28 @@
+import * as core from "@actions/core";
+import "fs";
+
+function run() {
+  const path = core.getInput("slsa-layout-file");
+  const hash = crypto.createHash("sha256").update("somedata").digest("hex");
+
+  const slsaLayout = {
+    version: 1,
+    attestations: [
+      {
+        name: "foo.intoto",
+        subjects: [
+          {
+            name: "foo",
+            digest: {
+              sha256: hash,
+            },
+          },
+        ],
+      },
+    ],
+  };
+
+  fs.writeFileSync(path, JSON.stringify(slsaLayout));
+}
+
+run();
