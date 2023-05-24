eed3ef6cfe50010e88cf9edb53c449adcec1861f
Ian Lewis <ianmlewis@gmail.com>
2023-05-24 05:33:54 +0000

    Add upload file
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- action/src/index.ts
+++ action/src/index.ts
@@ -1,19 +1,44 @@
+import * as artifact from "@actions/artifact";
 import * as core from "@actions/core";
 import * as crypto from "crypto";
 import * as fs from "fs";
 
 function run() {
+  // Write and upload the artifact file.
+  const filename = "foo";
+  const filedata = "somedata";
+
+  fs.writeFileSync(filename, filedata);
+
+  const artifactClient = artifact.create();
+  const uploadResponse = await artifactClient.uploadArtifact(
+    filename, // artifact name
+    filename // file name
+  );
+
+  if (uploadResponse.failedItems.length > 0) {
+    core.setFailed(
+      `An error was encountered when uploading ${uploadResponse.artifactName}. There were ${uploadResponse.failedItems.length} items that failed to upload.`
+    );
+  } else {
+    core.info(
+      `Artifact ${uploadResponse.artifactName} has been successfully uploaded!`
+    );
+  }
+
+  // generate the layout
+
   const path = core.getInput("slsa-layout-file");
-  const hash = crypto.createHash("sha256").update("somedata").digest("hex");
+  const hash = crypto.createHash("sha256").update(filedata).digest("hex");
 
   const slsaLayout = {
     version: 1,
     attestations: [
       {
-        name: "foo.intoto",
+        name: filename + ".intoto",
         subjects: [
           {
-            name: "foo",
+            name: filename,
             digest: {
               sha256: hash,
             },
