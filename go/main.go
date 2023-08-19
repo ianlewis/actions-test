23a4f1b07ceea6f82b7d1f32bcb7acb0d9951d7e
Ian Lewis <ianmlewis@gmail.com>
2023-08-19 09:02:37 +0000

    Add TODO
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- go/main.go
+++ go/main.go
@@ -3,6 +3,7 @@ package main
 import "net/http"
 
 func main() {
+	// TODO(#2): This issue is still open.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!\n"))
 	})
