9bcba137ba615f3e106311e8e82c3fbf65fedb4c
Ian Lewis <ianmlewis@gmail.com>
2024-02-12 13:04:46 +0000

    update publish
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- nodejs/publish.sh
+++ nodejs/publish.sh
@@ -2,4 +2,4 @@
 version="$(npm version patch)"
 git commit -asm "Update to ${version}" >/dev/null
 git tag "${version}" >/dev/null
-echo "git push origin main ${version}"
+git push origin main ${version}
